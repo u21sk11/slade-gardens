afbad3d1a7afbc110d35b3b49a5f86ba9089248c
Satas Kapocius <satas.kapocius.18@gmail.com>
2025-03-12 00:52:07 +0000

    chore: otpimise images

--- src/app/pages/About.jsx
+++ src/app/pages/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
       <div className="w-full h-48 md:h-64 lg:h-80 rounded-lg overflow-hidden flex items-center justify-center mb-8">
         <img
           className="w-full h-full object-cover"
-          src="./assets/slade-about.png"
+          src="./src/assets/slade-about.png"
           alt="Slade Gardens Adventure Playground"
         />
       </div>
