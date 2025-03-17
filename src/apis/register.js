ec982773f6ae5ec5395d1b8ed0417a549d21e6b2
CS-426 <craig.scott.20@hotmail.com>
2025-03-17 20:43:56 +0000

    Update register.js

--- src/apis/register.js
+++ src/apis/register.js
@@ -1,13 +1,37 @@
-import { v4 as uuidv4 } from "uuid";
 import { generateClient } from "aws-amplify/data";
 import outputs from "../../amplify_outputs.json";
 import { Amplify } from "aws-amplify";
+import { v4 as uuidv4 } from "uuid";
+import { auditCreate, auditError } from "./audit";
 
 Amplify.configure(outputs);
 const client = generateClient({
   authMode: "userPool",
 });
 
-export async function registerGuardian(newGuardian){
-    
+/**
+ * Registers a guardian and their children
+ */
+export async function registerGuardian(newGuardian, children) {
+  try {
+    const guardianCheck = await client.models.Playground.get({
+      childId: childId,
+    });
+    const { errors: checkError } = guardianCheck;
+    if (checkError) throw new Error(checkError[0].message);
+    if (guardianCheck) throw new Error("Guardian already registered");
+  
+    const guardianResponse = await client.models.Guardian.create({
+      guardianId: uuidv4(),
+      firstName: newGuardian.firstName,
+      lastName: newGuardian.lastName,
+    });
+    const { errors: guardianCreateError } = guardianResponse;
+    if (guardianCreateError) throw new Error(guardianCreateError[0].message);
+
+    auditCreate(guardianResponse.data.guardianId, null);
+
+  } catch (error) {
+    console.error("Error registering guardian: " + error);
+  }
 }
