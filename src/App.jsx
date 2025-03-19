59d638864c81d659dff13ac98c5124cc2a53bb90
Satas Kapocius <satas.kapocius.18@gmail.com>
2025-03-19 14:09:24 +0000

    feat: integrate authenticator

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ import { Routes, Route } from 'react-router-dom';
 import PrivacyPolicy from "./app/pages/PrivacyPolicy";
 import About from './app/pages/About';
 import Contact from "./app/pages/Contact";
-import RegistrationForm from "./app/pages/RegistrationForm";
+import Registration from "./app/pages/Registration";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
@@ -34,219 +34,26 @@ import Footer from "./components/Footer";
  */
 
 Amplify.configure(outputs);
-const client = generateClient({
-  authMode: "userPool",
-});
 
 export default function App() {
-  const [notes, setNotes] = useState([]);
-
-  useEffect(() => {
-    fetchNotes();
-  }, []);
-
-  async function fetchNotes() {
-    const { data: notes } = await client.models.Note.list();
-    await Promise.all(
-      notes.map(async (note) => {
-        if (note.image) {
-          const linkToStorageFile = await getUrl({
-            path: ({ identityId }) => `media/${identityId}/${note.image}`,
-          });
-          console.log(linkToStorageFile.url);
-          note.image = linkToStorageFile.url;
-        }
-        return note;
-      })
-    );
-    console.log(notes);
-    setNotes(notes);
-  }
-
-  async function createNote(event) {
-    event.preventDefault();
-    const form = new FormData(event.target);
-    console.log(form.get("image").name);
-
-    const { data: newNote } = await client.models.Note.create({
-      name: form.get("name"),
-      description: form.get("description"),
-      image: form.get("image").name,
-    });
-
-    console.log(newNote);
-    if (newNote.image)
-      if (newNote.image)
-        await uploadData({
-          path: ({ identityId }) => `media/${identityId}/${newNote.image}`,
-
-          data: form.get("image"),
-        }).result;
-
-    fetchNotes();
-    event.target.reset();
-  }
-
-  async function deleteNote({ id }) {
-    const toBeDeletedNote = {
-      id: id,
-    };
-
-    const { data: deletedNote } = await client.models.Note.delete(
-      toBeDeletedNote
-    );
-    console.log(deletedNote);
-
-    fetchNotes();
-  }
-
-  async function handleSubmit(event) {
-    const form = new FormData(event.target);
-    event.preventDefault();
-    await createTestData(form);
-  }
-
-  async function handlePlaygroundEntry() {
-    await enterPlayground("1234");
-  }
-
-  async function handlePlaygroundExit() {
-    await exitPlayground("1234");
-  }
-
-  async function handleAuditCreate() {
-    await auditCreate("1234", "5678");
-    await auditCreate(null, "5678");
-    await auditCreate("1234", "5678");
-  }
-  
-  async function handleSeedEmojiStore() {
-    seedEmojiStore();
-  }
-  
-  async function handleAssignEmoji() {
-    getUnassignedEmojis(5);
-  }
-  
-  async function handleCheckEmoji() {
-    getChildId("🍔🍕🍟");
-  }
 
   return (
-    <div className="flex flex-col min-h-screen">
-      <Navbar />
-      <div className="flex-grow">
-        <Routes>
-          <Route path="/privacy-policy" element={<PrivacyPolicy />}/>
-          <Route path="/about" element={<About />}/>
-          <Route path="/contact" element={<Contact />}/>
-          <Route path="/register" element={<RegistrationForm />}/>
-        </Routes>
-      </div>
-      <Footer className="flex-shrink-0" />
-    {/* <Authenticator>
-      {({ signOut }) => (
-        <Flex
-          className="App"
-          justifyContent="center"
-          alignItems="center"
-          direction="column"
-          width="70%"
-          margin="0 auto"
-        >
-          
-          <Heading level={1}>My Notes App</Heading>
-          <View as="form" margin="3rem 0" onSubmit={handleSubmit}>
-            <Flex
-              direction="column"
-              justifyContent="center"
-              gap="2rem"
-              padding="2rem"
-            >
-              <TextField
-                name="name"
-                placeholder="Note Name"
-                label="Note Name"
-                labelHidden
-                variation="quiet"
-                required
-              />
-              <TextField
-                name="description"
-                placeholder="Note Description"
-                label="Note Description"
-                labelHidden
-                variation="quiet"
-                required
-              />
-              <Button type="submit" variation="primary">
-                Create Note
-              </Button>
-            </Flex>
-          </View>
-          <Divider />
-          <Heading level={2}>Current Notes</Heading>
-          <Grid
-            margin="3rem 0"
-            autoFlow="column"
-            justifyContent="center"
-            gap="2rem"
-            alignContent="center"
-          >
-            {notes.map((note) => (
-              <Flex
-                key={note.id || note.name}
-                direction="column"
-                justifyContent="center"
-                alignItems="center"
-                gap="2rem"
-                border="1px solid #ccc"
-                padding="2rem"
-                borderRadius="5%"
-                className="box"
-              >
-                <View>
-                  <Heading level="3">{note.name}</Heading>
-                </View>
-                <Text fontStyle="italic">{note.description}</Text>
-                {note.image && (
-                  <Image
-                    src={note.image}
-                    alt={`visual aid for ${notes.name}`}
-                    style={{ width: 400 }}
-                  />
-                )}
-                <Button
-                  variation="destructive"
-                  onClick={() => deleteNote(note)}
-                >
-                  Delete note
-                </Button>
-              </Flex>
-            ))}
-          </Grid>
-          <Button onClick={handlePlaygroundEntry} variation="link">
-            Playground ENTRY Test
-          </Button>
-          <Button onClick={handlePlaygroundExit} variation="link">
-            Playground EXIT Test
-          </Button>
-          <Button onClick={handleAuditCreate} variation="link">
-            Audit CREATE Test
-          </Button>
-          <Button onClick={handleSeedEmojiStore} variation="destructive">
-            SEED EMOJISTORE
-          </Button>
-          <Button onClick={handleAssignEmoji} variation="warning">
-            ASSIGN EMOJI
-          </Button>
-          <Button onClick={handleCheckEmoji} variation="warning">
-            CHECK EMOJI
-          </Button>
-          <Button onClick={signOut}>Sign Out</Button>
-        </Flex>
-      )}
-    </Authenticator> */}
+             <div className="flex flex-col min-h-screen">
+             <Navbar />
+
+            {/* <Button onClick={signOut}>Sign Out</Button> */}
+            <div className="flex-grow">
+              <Routes>
+                <Route path="/privacy-policy" element={<PrivacyPolicy />}/>
+                <Route path="/about" element={<About />}/>
+                <Route path="/contact" element={<Contact />}/>
+                <Route path="/register" element={<Registration />}/>
+              </Routes>
+            </div>
+
+            
+           
+    <Footer className="flex-shrink-0" />
     </div>
   );
 }
---
+++ src/app/pages/Registration.jsx
@@ -0,0 +1,523 @@
+import React, { useState } from "react";
+import { register } from "../../apis/register";
+import { Authenticator } from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
+import "@aws-amplify/ui-react/styles.css";
+import outputs from "../../../amplify_outputs.json";
+
+Amplify.configure(outputs);
+
+function Registration() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [addressLine1, setAddressLine1] = useState("");
+  const [addressLine2, setAddressLine2] = useState("");
+  const [city, setCity] = useState("");
+  const [postcode, setPostcode] = useState("");
+  const [role, setRole] = useState("");
+  const [error, setError] = useState("");
+
+  // ---------------------------- FOR GUARDIAN REGISTRATION ----------------------------
+
+  const [children, setChildren] = useState([
+    {
+      firstName: "",
+      lastName: "",
+      gender: "",
+      ethnicity: "",
+      dob: "",
+      school: "",
+      allergies: "",
+      specialNeeds: "",
+    },
+  ]);
+  const [permissions, setPermissions] = useState({
+    photos: "",
+    emails: "",
+    terms: "",
+  });
+  const [referralSource, setReferralSource] = useState("");
+
+  const handleChildChange = (index, field, value) => {
+    const updatedChildren = [...children];
+    updatedChildren[index][field] = value;
+    setChildren(updatedChildren);
+  };
+
+  const addChild = () => {
+    setChildren([
+      ...children,
+      {
+        firstName: "",
+        lastName: "",
+        gender: "",
+        ethnicity: "",
+        dob: "",
+        school: "",
+        allergies: "",
+        specialNeeds: "",
+      },
+    ]);
+  };
+
+  const removeChild = (index) => {
+    const updatedChildren = children.filter((_, i) => i !== index);
+    setChildren(updatedChildren);
+  };
+
+  // --------------------------------------------------------------------------------
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newGuardian = {
+      firstName,
+      lastName,
+      email,
+      addressLine1,
+      addressLine2,
+      city,
+      postcode,
+      phoneNumber,
+      permissions,
+      referralSource
+    };
+    console.log("New Guardian Registered:", newGuardian);
+    console.log("Children:", children);
+
+    // Call the register function from the API
+    register(newGuardian, children);
+  };
+
+  return (
+    <Authenticator>
+       {({ signOut, user }) => (
+        <div className="py-10 bg-white p-10 rounded-lg shadow-md w-full max-w-4xl mx-auto">
+        {/* Title and Disclaimer Text */}
+        <h2 className="text-3xl font-semibold text-center text-[#222831] mb-4">
+          Register
+        </h2>
+        <p className="text-sm text-center text-[#6FB545] mb-6">
+          Please note: Slade Gardens Adventure Playground is not a childcare
+          facility.
+        </p>
+  
+        {/* Registration Form */}
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4 flex space-x-6">
+            <div className="flex-1">
+              <label
+                htmlFor="firstName"
+                className="block text-[#222831] font-medium"
+              >
+                First Name:
+              </label>
+              <input
+                type="text"
+                id="firstName"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+                className="w-full p-3 mt-2 border border-[#6FB545] rounded-md focus:outline-none focus:ring-2 focus:ring-[#F9DE3F]"
+                required
+              />
+            </div>
+            <div className="flex-1">
+              <label
+                htmlFor="lastName"
+                className="block text-[#222831] font-medium"
+              >
+                Last Name:
+              </label>
+              <input
+                type="text"
+                id="lastName"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                className="w-full p-3 mt-2 border border-[#6FB545] rounded-md focus:outline-none focus:ring-2 focus:ring-[#F9DE3F]"
+                required
+              />
+            </div>
+          </div>
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-[#222831] font-medium">
+              Email Address:
+            </label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="w-full p-3 mt-2 border border-[#6FB545] rounded-md focus:outline-none focus:ring-2 focus:ring-[#F9DE3F]"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="phoneNumber" className="block text-[#222831] font-medium">
+              Phone Number:
+            </label>
+            <input
+              type="tel"
+              id="phoneNumber"
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+              className="w-full p-3 mt-2 border border-[#6FB545] rounded-md focus:outline-none focus:ring-2 focus:ring-[#F9DE3F]"
+              required
+            />
+          </div>
+  
+          {/* Address Fields */}
+          <div className="mb-4">
+            <label
+              htmlFor="addressLine1"
+              className="block text-[#222831] font-medium"
+            >
+              Address Line 1:
+            </label>
+            <input
+              type="text"
+              id="addressLine1"
+              value={addressLine1}
+              onChange={(e) => setAddressLine1(e.target.value)}
+              className="w-full p-3 mt-2 border border-[#6FB545] rounded-md focus:outline-none focus:ring-2 focus:ring-[#F9DE3F]"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="addressLine2"
+              className="block text-[#222831] font-medium"
+            >
+              Address Line 2:
+            </label>
+            <input
+              type="text"
+              id="addressLine2"
+              value={addressLine2}
+              onChange={(e) => setAddressLine2(e.target.value)}
+              className="w-full p-3 mt-2 border border-[#6FB545] rounded-md focus:outline-none focus:ring-2 focus:ring-[#F9DE3F]"
+            />
+          </div>
+          <div className="mb-4 flex space-x-6">
+            <div className="flex-1">
+              <label htmlFor="city" className="block text-[#222831] font-medium">
+                City:
+              </label>
+              <input
+                type="text"
+                id="city"
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+                className="w-full p-3 mt-2 border border-[#6FB545] rounded-md focus:outline-none focus:ring-2 focus:ring-[#F9DE3F]"
+                required
+              />
+            </div>
+            <div className="flex-1">
+              <label
+                htmlFor="postcode"
+                className="block text-[#222831] font-medium"
+              >
+                Postcode:
+              </label>
+              <input
+                type="text"
+                id="postcode"
+                value={postcode}
+                onChange={(e) => setPostcode(e.target.value)}
+                className="w-full p-3 mt-2 border border-[#6FB545] rounded-md focus:outline-none focus:ring-2 focus:ring-[#F9DE3F]"
+                required
+              />
+            </div>
+          </div>
+  
+          {/* Fields for Role Selection */}
+          <div className="mb-4">
+                      <label htmlFor="role" className="block text-[#222831] font-medium">
+                          Select Role:
+                      </label>
+                      <select
+                          id="role"
+                          value={role}
+                          onChange={(e) => setRole(e.target.value)}
+                          className="w-full p-3 mt-2 border border-[#6FB545] rounded-md focus:outline-none focus:ring-2 focus:ring-[#F9DE3F]"
+                          required
+                      >
+                          <option value="">-- Select Role --</option>
+                          <option value="guardian">Guardian</option>
+                          <option value="volunteer">Volunteer</option>
+                      </select>
+                  </div>
+  
+          {/* Additional Fields for Guardian Registration */}
+          {role === "guardian" && (
+            <>
+              {children.map((child, index) => (
+                <div key={index} className="mb-6 border-t border-gray-300 pt-4">
+                  <h3 className="text-lg font-semibold text-gray-700 mb-4">
+                    Child {index + 1}
+                    {children.length > 1 && (
+                      <button
+                        type="button"
+                        onClick={() => removeChild(index)}
+                        className="ml-4 text-red-500 text-sm hover:underline"
+                      >
+                        Remove
+                      </button>
+                    )}
+                  </h3>
+                  <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                    <input
+                      type="text"
+                      placeholder="First Name*"
+                      value={child.firstName}
+                      onChange={(e) =>
+                        handleChildChange(index, "firstName", e.target.value)
+                      }
+                      className="p-3 border border-gray-300 rounded-md"
+                      required
+                    />
+                    <input
+                      type="text"
+                      placeholder="Last Name*"
+                      value={child.lastName}
+                      onChange={(e) =>
+                        handleChildChange(index, "lastName", e.target.value)
+                      }
+                      className="p-3 border border-gray-300 rounded-md"
+                      required
+                    />
+                    <select
+                      value={child.gender}
+                      onChange={(e) =>
+                        handleChildChange(index, "gender", e.target.value)
+                      }
+                      className="p-3 border border-gray-300 rounded-md"
+                      required
+                    >
+                      <option value="">Gender*</option>
+                      <option value="MALE">Male</option>
+                      <option value="FEMALE">Female</option>
+                      <option value="NONBINARY">Non-Binary</option>
+                      <option value="OTHER">Other</option>
+                    </select>
+                    <select
+                      value={child.ethnicity}
+                      onChange={(e) =>
+                        handleChildChange(index, "ethnicity", e.target.value)
+                      }
+                      className="p-3 border border-gray-300 rounded-md"
+                      required
+                    >
+                      <option value="">Ethnicity*</option>
+                      <option value="white">White</option>
+                      <option value="black">Black</option>
+                      <option value="asian">Asian</option>
+                      <option value="mixed">Mixed</option>
+                      <option value="other">Other</option>
+                    </select>
+                    <input
+                      type="date"
+                      placeholder="Date of Birth*"
+                      value={child.dob}
+                      onChange={(e) =>
+                        handleChildChange(index, "dob", e.target.value)
+                      }
+                      className="p-3 border border-gray-300 rounded-md"
+                      required
+                    />
+                    <input
+                      type="text"
+                      placeholder="School*"
+                      value={child.school}
+                      onChange={(e) =>
+                        handleChildChange(index, "school", e.target.value)
+                      }
+                      className="p-3 border border-gray-300 rounded-md"
+                      required
+                    />
+                    <input
+                      type="text"
+                      placeholder="Any Allergies?"
+                      value={child.allergies}
+                      onChange={(e) =>
+                        handleChildChange(index, "allergies", e.target.value)
+                      }
+                      className="p-3 border border-gray-300 rounded-md"
+                    />
+                    <input
+                      type="text"
+                      placeholder="Disabilities / Special Needs?"
+                      value={child.specialNeeds}
+                      onChange={(e) =>
+                        handleChildChange(index, "specialNeeds", e.target.value)
+                      }
+                      className="p-3 border border-gray-300 rounded-md"
+                    />
+                  </div>
+                </div>
+              ))}
+  
+              <button
+                type="button"
+                onClick={addChild}
+                className="bg-yellow-500 text-white px-4 py-2 rounded-md hover:bg-yellow-600 focus:outline-none"
+              >
+                + Add Child
+              </button>
+              {/* Permissions */}
+              <div className="mt-6">
+                <h3 className="text-lg font-semibold text-gray-700 mb-2">
+                  Permissions
+                </h3>
+                <div className="mb-4">
+                  <label className="block text-gray-700">
+                    Do we have your permission for your child's photographs/videos
+                    to be used on our social media or marketing?
+                  </label>
+                  <div className="flex items-center space-x-4">
+                    <label>
+                      <input
+                        type="radio"
+                        name="photos"
+                        value="yes"
+                        checked={permissions.photos === "yes"}
+                        onChange={(e) =>
+                          setPermissions({
+                            ...permissions,
+                            photos: e.target.value,
+                          })
+                        }
+                      />{" "}
+                      Yes
+                    </label>
+                    <label>
+                      <input
+                        type="radio"
+                        name="photos"
+                        value="no"
+                        checked={permissions.photos === "no"}
+                        onChange={(e) =>
+                          setPermissions({
+                            ...permissions,
+                            photos: e.target.value,
+                          })
+                        }
+                      />{" "}
+                      No
+                    </label>
+                  </div>
+                </div>
+                <div className="mb-4">
+                  <label className="block text-gray-700">
+                    May we occasionally email you with news and notices of our
+                    community events?
+                  </label>
+                  <div className="flex items-center space-x-4">
+                    <label>
+                      <input
+                        type="radio"
+                        name="emails"
+                        value="yes"
+                        checked={permissions.emails === "yes"}
+                        onChange={(e) =>
+                          setPermissions({
+                            ...permissions,
+                            emails: e.target.value,
+                          })
+                        }
+                      />{" "}
+                      Yes
+                    </label>
+                    <label>
+                      <input
+                        type="radio"
+                        name="emails"
+                        value="no"
+                        checked={permissions.emails === "no"}
+                        onChange={(e) =>
+                          setPermissions({
+                            ...permissions,
+                            emails: e.target.value,
+                          })
+                        }
+                      />{" "}
+                      No
+                    </label>
+                  </div>
+                </div>
+                <div className="mb-4">
+                  <label className="block text-gray-700">
+                    Do you agree to the{" "}
+                    <a href="#" className="text-blue-500 hover:underline">
+                      terms and conditions?
+                    </a>
+                  </label>
+                  <div className="flex items-center space-x-4">
+                    <label>
+                      <input
+                        type="radio"
+                        name="terms"
+                        value="yes"
+                        checked={permissions.terms === "yes"}
+                        onChange={(e) =>
+                          setPermissions({
+                            ...permissions,
+                            terms: e.target.value,
+                          })
+                        }
+                      />{" "}
+                      Yes
+                    </label>
+                    <label>
+                      <input
+                        type="radio"
+                        name="terms"
+                        value="no"
+                        checked={permissions.terms === "no"}
+                        onChange={(e) =>
+                          setPermissions({
+                            ...permissions,
+                            terms: e.target.value,
+                          })
+                        }
+                      />{" "}
+                      No
+                    </label>
+                  </div>
+                </div>
+              </div>
+  
+              {/* How did you hear */}
+              <div className="mb-6">
+                <label htmlFor="referralSource" className="block text-gray-700">
+                  How did you hear about Slade Gardens?
+                </label>
+                <input
+                  type="text"
+                  id="referralSource"
+                  value={referralSource}
+                  onChange={(e) => setReferralSource(e.target.value)}
+                  className="w-full p-3 mt-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-yellow-500"
+                />
+              </div>
+            </>
+          )}
+  
+          {error && <p className="text-red-500 text-center mb-4">{error}</p>}
+  
+          <button
+            type="submit"
+            className="w-full py-3 mt-4 bg-[#6FB545] text-white rounded-md hover:bg-[#078543] focus:outline-none"
+          >
+            Register
+          </button>
+        </form>
+      </div>
+)}
+    
+    </Authenticator>
+  );
+}
+
+export default Registration;
