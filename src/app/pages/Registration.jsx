92c5f480dddb54733ad869aa058d8916805f8675
CS-426 <craig.scott.20@hotmail.com>
2025-03-22 16:12:16 +0000

    Update Registration.jsx
    Refinements and validation

--- src/app/pages/Registration.jsx
+++ src/app/pages/Registration.jsx
@@ -128,9 +128,19 @@ function Registration() {
 
   const ProgressBar = ({ step }) => (
     <div className="flex justify-between mb-6">
-      <div className={`flex-1 ${step >= 1 ? 'bg-green-500' : 'bg-gray-300'} h-2 rounded-l-lg`}></div>
-      <div className={`flex-1 ${step >= 2 ? 'bg-green-500' : 'bg-gray-300'} h-2`}></div>
-      <div className={`flex-1 ${step >= 3 ? 'bg-green-500' : 'bg-gray-300'} h-2 rounded-r-lg`}></div>
+      <div
+        className={`flex-1 ${
+          step >= 1 ? "bg-green-500" : "bg-gray-300"
+        } h-2 rounded-l-lg`}
+      ></div>
+      <div
+        className={`flex-1 ${step >= 2 ? "bg-green-500" : "bg-gray-300"} h-2`}
+      ></div>
+      <div
+        className={`flex-1 ${
+          step >= 3 ? "bg-green-500" : "bg-gray-300"
+        } h-2 rounded-r-lg`}
+      ></div>
     </div>
   );
 
@@ -154,29 +164,45 @@ function Registration() {
       setStep((prevStep) => Math.min(prevStep + 1, 3));
     }
   };
-  
+
   const handlePrevious = () => {
     setStep((prevStep) => Math.max(prevStep - 1, 1));
   };
 
   const validateStep1 = () => {
-    if (!firstName || !lastName || !phoneNumber || !addressLine1 || !city || !postcode) {
+    if (
+      !firstName ||
+      !lastName ||
+      !phoneNumber ||
+      !addressLine1 ||
+      !city ||
+      !postcode
+    ) {
       setError("Please fill in all required fields.");
       return false;
     }
     return true;
   };
-  
+
   const validateStep2 = () => {
     for (const child of children) {
-      if (!child.firstName || !child.lastName || !child.gender || !child.ethnicity || !child.dob || !child.school || !child.permissionToLeave || !child.freeSchoolMeals) {
+      if (
+        !child.firstName ||
+        !child.lastName ||
+        !child.gender ||
+        !child.ethnicity ||
+        !child.dob ||
+        !child.school ||
+        !child.permissionToLeave ||
+        !child.freeSchoolMeals
+      ) {
         setError("Please fill in all required fields for each child.");
         return false;
       }
     }
     return true;
   };
-  
+
   const validateStep3 = () => {
     if (!permissions.photos || !permissions.emails || !permissions.terms) {
       setError("Please fill in all required fields.");
@@ -325,34 +351,34 @@ function Registration() {
               </div>
             </div>
 
-          <div className="flex justify-center mt-6">
-            {step > 1 && (
-              <button
-                type="button"
-                onClick={handlePrevious}
-                className="bg-gray-300 text-gray-700 px-4 py-2 rounded-md hover:bg-gray-400 focus:outline-none"
-              >
-                Previous
-              </button>
-            )}
-            {step < 3 && (
-              <button
-                type="button"
-                onClick={handleNext}
-                className="bg-[#6FB545] text-white px-4 py-2 rounded-md hover:bg-[#078543] focus:outline-none"
-              >
-                Next
-              </button>
-            )}
-          </div>
+            <div className="flex justify-center mt-6">
+              {step > 1 && (
+                <button
+                  type="button"
+                  onClick={handlePrevious}
+                  className="bg-gray-300 text-gray-700 px-4 py-2 rounded-md hover:bg-gray-400 focus:outline-none"
+                >
+                  Previous
+                </button>
+              )}
+              {step < 3 && (
+                <button
+                  type="button"
+                  onClick={handleNext}
+                  className="bg-[#6FB545] text-white px-4 py-2 rounded-md hover:bg-[#078543] focus:outline-none"
+                >
+                  Next
+                </button>
+              )}
+            </div>
           </div>
         );
       case 2:
         return (
           <div>
             {/* Child details form fields */}
-                        {/* Additional Fields for Guardian Registration */}
-                        {
+            {/* Additional Fields for Guardian Registration */}
+            {
               <>
                 {children.map((child, index, guardianId) => (
                   <div
@@ -432,6 +458,7 @@ function Registration() {
                         }
                         className="p-3 border border-gray-300 rounded-md"
                         required
+                        onKeyDown={(e) => e.preventDefault()}
                       />
                       <select
                         value={child.permissionToLeave}
@@ -513,184 +540,178 @@ function Registration() {
               </>
             }
 
-          <div className="flex justify-center mt-6">
-            {step > 1 && (
-              <button
-                type="button"
-                onClick={handlePrevious}
-                className="bg-gray-300 text-gray-700 px-4 py-2 rounded-xl hover:bg-gray-400 focus:outline-none mr-4"
-              >
-                Previous
-              </button>
-            )}
-            {step < 3 && (
-              <button
-                type="button"
-                onClick={handleNext}
-                className="bg-[#6FB545] text-white px-4 py-2 rounded-xl hover:bg-[#078543] focus:outline-none ml-4"
-              >
-                Next
-              </button>
-            )}
-          </div>
-
+            <div className="flex justify-center mt-6">
+              {step > 1 && (
+                <button
+                  type="button"
+                  onClick={handlePrevious}
+                  className="bg-gray-300 text-gray-700 px-4 py-2 rounded-xl hover:bg-gray-400 focus:outline-none mr-4"
+                >
+                  Previous
+                </button>
+              )}
+              {step < 3 && (
+                <button
+                  type="button"
+                  onClick={handleNext}
+                  className="bg-[#6FB545] text-white px-4 py-2 rounded-xl hover:bg-[#078543] focus:outline-none ml-4"
+                >
+                  Next
+                </button>
+              )}
+            </div>
           </div>
         );
       case 3:
         return (
           <div>
             {/* Additional details form fields */}
-            
-                {/* Permissions */}
-                <div className="mt-6">
-                  <h3 className="text-lg font-semibold text-gray-700 mb-2">
-                    Permissions
-                  </h3>
-                  <div className="mb-4">
-                    <label className="block text-gray-700">
-                      Do we have your permission for your child's
-                      photographs/videos to be used on our social media or
-                      marketing?*
-                    </label>
-                    <div className="flex items-center space-x-4">
-                      <label>
-                        <input
-                          type="radio"
-                          name="photos"
-                          value="yes"
-                          checked={permissions.photos === "yes"}
-                          onChange={(e) =>
-                            setPermissions({
-                              ...permissions,
-                              photos: e.target.value,
-                            })
-                          }
-                          required
-                        />{" "}
-                        Yes
-                      </label>
-                      <label>
-                        <input
-                          type="radio"
-                          name="photos"
-                          value="no"
-                          checked={permissions.photos === "no"}
-                          onChange={(e) =>
-                            setPermissions({
-                              ...permissions,
-                              photos: e.target.value,
-                            })
-                          }
-                          required
-                        />{" "}
-                        No
-                      </label>
-                    </div>
-                  </div>
-                  <div className="mb-4">
-                    <label className="block text-gray-700">
-                      May we occasionally email you with news and notices of our
-                      community events?*
-                    </label>
-                    <div className="flex items-center space-x-4">
-                      <label>
-                        <input
-                          type="radio"
-                          name="emails"
-                          value="yes"
-                          checked={permissions.emails === "yes"}
-                          onChange={(e) =>
-                            setPermissions({
-                              ...permissions,
-                              emails: e.target.value,
-                            })
-                          }
-                          required
-                        />{" "}
-                        Yes
-                      </label>
-                      <label>
-                        <input
-                          type="radio"
-                          name="emails"
-                          value="no"
-                          checked={permissions.emails === "no"}
-                          onChange={(e) =>
-                            setPermissions({
-                              ...permissions,
-                              emails: e.target.value,
-                            })
-                          }
-                          required
-                        />{" "}
-                        No
-                      </label>
-                    </div>
-                  </div>
-                  <div className="mb-4">
-                    <label className="block text-gray-700">
-                      Do you agree to the{" "}
-                      <a href="#" className="text-blue-500 hover:underline">
-                        terms and conditions
-                      </a>
-                      ?*
-                    </label>
-                    <div className="flex items-center space-x-4">
-                      <label>
-                        <input
-                          type="radio"
-                          name="terms"
-                          value="yes"
-                          checked={permissions.terms === "yes"}
-                          onChange={(e) =>
-                            setPermissions({
-                              ...permissions,
-                              terms: e.target.value,
-                            })
-                          }
-                          required
-                        />{" "}
-                        Yes
-                      </label>
-                      <label>
-                        <input
-                          type="radio"
-                          name="terms"
-                          value="no"
-                          checked={permissions.terms === "no"}
-                          onChange={(e) =>
-                            setPermissions({
-                              ...permissions,
-                              terms: e.target.value,
-                            })
-                          }
-                          required
-                        />{" "}
-                        No
-                      </label>
-                    </div>
-                  </div>
-                </div>
 
-                {/* How did you hear */}
-                <div className="mb-6">
-                  <label
-                    htmlFor="referralSource"
-                    className="block text-gray-700"
-                  >
-                    How did you hear about Slade Gardens?
+            {/* Permissions */}
+            <div className="mt-6">
+              <h3 className="text-lg font-semibold text-gray-700 mb-2">
+                Permissions
+              </h3>
+              <div className="mb-4">
+                <label className="block text-gray-700">
+                  Do we have your permission for your child's photographs/videos
+                  to be used on our social media or marketing?*
+                </label>
+                <div className="flex items-center space-x-4">
+                  <label>
+                    <input
+                      type="radio"
+                      name="photos"
+                      value="yes"
+                      checked={permissions.photos === "yes"}
+                      onChange={(e) =>
+                        setPermissions({
+                          ...permissions,
+                          photos: e.target.value,
+                        })
+                      }
+                      required
+                    />{" "}
+                    Yes
                   </label>
-                  <input
-                    type="text"
-                    id="referralSource"
-                    value={referralSource}
-                    onChange={(e) => setReferralSource(e.target.value)}
-                    className="w-full p-3 mt-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-yellow-500"
-                  />
+                  <label>
+                    <input
+                      type="radio"
+                      name="photos"
+                      value="no"
+                      checked={permissions.photos === "no"}
+                      onChange={(e) =>
+                        setPermissions({
+                          ...permissions,
+                          photos: e.target.value,
+                        })
+                      }
+                      required
+                    />{" "}
+                    No
+                  </label>
                 </div>
+              </div>
+              <div className="mb-4">
+                <label className="block text-gray-700">
+                  May we occasionally email you with news and notices of our
+                  community events?*
+                </label>
+                <div className="flex items-center space-x-4">
+                  <label>
+                    <input
+                      type="radio"
+                      name="emails"
+                      value="yes"
+                      checked={permissions.emails === "yes"}
+                      onChange={(e) =>
+                        setPermissions({
+                          ...permissions,
+                          emails: e.target.value,
+                        })
+                      }
+                      required
+                    />{" "}
+                    Yes
+                  </label>
+                  <label>
+                    <input
+                      type="radio"
+                      name="emails"
+                      value="no"
+                      checked={permissions.emails === "no"}
+                      onChange={(e) =>
+                        setPermissions({
+                          ...permissions,
+                          emails: e.target.value,
+                        })
+                      }
+                      required
+                    />{" "}
+                    No
+                  </label>
+                </div>
+              </div>
+              <div className="mb-4">
+                <label className="block text-gray-700">
+                  Do you agree to the{" "}
+                  <a href="#" className="text-blue-500 hover:underline">
+                    terms and conditions
+                  </a>
+                  ?*
+                </label>
+                <div className="flex items-center space-x-4">
+                  <label>
+                    <input
+                      type="radio"
+                      name="terms"
+                      value="yes"
+                      checked={permissions.terms === "yes"}
+                      onChange={(e) =>
+                        setPermissions({
+                          ...permissions,
+                          terms: e.target.value,
+                        })
+                      }
+                      required
+                    />{" "}
+                    Yes
+                  </label>
+                  <label>
+                    <input
+                      type="radio"
+                      name="terms"
+                      value="no"
+                      checked={permissions.terms === "no"}
+                      onChange={(e) =>
+                        setPermissions({
+                          ...permissions,
+                          terms: e.target.value,
+                        })
+                      }
+                      required
+                    />{" "}
+                    No
+                  </label>
+                </div>
+              </div>
+            </div>
+
+            {/* How did you hear */}
+            <div className="mb-6">
+              <label htmlFor="referralSource" className="block text-gray-700">
+                How did you hear about Slade Gardens?
+              </label>
+              <input
+                type="text"
+                id="referralSource"
+                value={referralSource}
+                onChange={(e) => setReferralSource(e.target.value)}
+                className="w-full p-3 mt-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-yellow-500"
+              />
+            </div>
 
             <div className="flex justify-center mt-6">
-
               <button
                 type="button"
                 onClick={handlePrevious}
@@ -698,7 +719,7 @@ function Registration() {
               >
                 Previous
               </button>
-              </div>
+            </div>
 
             <button
               type="submit"
@@ -733,11 +754,8 @@ function Registration() {
             <ProgressBar step={step} />
             {renderStepContent(step, user)}
             {/* Additional Fields for Guardian Registration */}
-            
 
             {error && <p className="text-red-500 text-center mb-4">{error}</p>}
-
-            
           </form>
         </div>
       )}
