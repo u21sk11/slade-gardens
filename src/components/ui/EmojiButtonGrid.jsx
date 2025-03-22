922c13ce0bc04e16770ce70c1d530aac202fa6ea
CS-426 <craig.scott.20@hotmail.com>
2025-03-22 22:28:49 +0000

    Emojis to json file

--- src/components/ui/EmojiButtonGrid.jsx
+++ src/components/ui/EmojiButtonGrid.jsx
@@ -1,28 +1,22 @@
 import React from 'react';
 import EmojiButton from '../form/EmojiButton'
+import { useEffect, useState } from 'react';
+import emojiData from '../../data/emojis.json';
 
 function EmojiButtonGrid(props) {
     const { onEmojiClick } = props;
 
-    const unicodeData = [
-        { id: 1, unicode: '🍌', alt: 'Banana' },
-        { id: 2, unicode: '🕶️', alt: 'Sunglasses' },
-        { id: 3, unicode: '⏰', alt: 'Clock' },
-        { id: 4, unicode: '✈️', alt: 'Airplane' },
-        { id: 5, unicode: '🪴', alt: 'Plant' },
-        { id: 6, unicode: '🍗', alt: 'Chicken' },
-        { id: 7, unicode: '👕', alt: 'Shirt' },
-        { id: 8, unicode: '⭐', alt: 'Star' },
-        { id: 9, unicode: '🥄', alt: 'Spoon' },
-        { id: 10, unicode: '📦', alt: 'Box' },
-        { id: 11, unicode: '👢', alt: 'Boots' },
-        { id: 12, unicode: '🌳', alt: 'Tree' },
-        { id: 13, unicode: '🧸', alt: 'Teddy Bear' },
-        { id: 14, unicode: '🚗', alt: 'Car' },
-        { id: 15, unicode: '🍕', alt: 'Pizza' },
-        { id: 16, unicode: '🎺', alt: 'Trumpet' },
-    ];
+    const [unicodeData, setUnicodeData] = useState([]);
 
+    useEffect(() => {
+        const data = Object.keys(emojiData).map((key, index) => ({
+            id: index + 1,
+            unicode: key,
+            alt: emojiData[key]
+        }));
+        setUnicodeData(data);
+    }, []);
+    
     return (
         <div className="grid grid-cols-8 gap-4 p-4">
             {unicodeData.map((item) => (
