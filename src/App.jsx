0fa08891bcc75662de0a006c29277661eb541a2a
CS-426 <craig.scott.20@hotmail.com>
2025-03-22 19:09:53 +0000

    Admin

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,6 @@
-import { useState, useEffect } from "react";
-import {
-  Authenticator,
-  Button,
-  Text,
-  TextField,
-  Heading,
-  Flex,
-  View,
-  Image,
-  Grid,
-  Divider,
-} from "@aws-amplify/ui-react";
 import { Amplify } from "aws-amplify";
 import "@aws-amplify/ui-react/styles.css";
-import { getUrl } from "aws-amplify/storage";
-import { uploadData } from "aws-amplify/storage";
-import { generateClient } from "aws-amplify/data";
 import outputs from "../amplify_outputs.json";
-import { auditCreate } from "./apis/audit";
-import { enterPlayground, exitPlayground } from "./apis/playground";
-import { getUnassignedEmojis, getChildId, seedEmojiStore } from "./apis/emojiStore";
 
 import { Routes, Route } from 'react-router-dom';
 import PrivacyPolicy from "./app/pages/PrivacyPolicy";
@@ -29,6 +10,7 @@ import Registration from "./app/pages/Registration";
 import Volunteer from "./app/pages/Volunteer";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Admin from "./app/pages/Admin";
 
 /**
  * @type {import('aws-amplify/data').Client<import('../amplify/data/resource').Schema>}
@@ -45,6 +27,7 @@ export default function App() {
             
             <div className="flex-grow">
               <Routes>
+                <Route path="/admin" element={<Admin />} />
                 <Route path="/privacy-policy" element={<PrivacyPolicy />}/>
                 <Route path="/about" element={<About />}/>
                 <Route path="/contact" element={<Contact />}/>
