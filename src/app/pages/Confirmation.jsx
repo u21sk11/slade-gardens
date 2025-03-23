1c5f89675e2ef7a429614920a66c5eb23088dac7
CS-426 <craig.scott.20@hotmail.com>
2025-03-23 14:41:50 +0000

    Update Confirmation.jsx

--- src/app/pages/Confirmation.jsx
+++ src/app/pages/Confirmation.jsx
@@ -16,28 +16,28 @@ const Confirmation = () => {
     );
   }, [location.state]);
 
-  return (
+return (
     <div className="min-h-[60vh] flex items-center justify-center py-5">
-      <div className="bg-white p-6 rounded-lg shadow-md w-full max-w-4xl">
-        <h1 className="text-3xl font-galindo font-bold text-sladeOrange text-center mb-1">
-          Registration Successful!
-        </h1>
-        <br />
-        <p className="font-galindo text-center">
-          Thank you for registering. Here are the login details for your
-          children:
-        </p>
-        <ul>
-          {firstNames.map((name, index) => (
-            <li key={`${name}-${index}`}>
-              <span className="font-bold">{name}</span>
-              {emojis[index] && <div>{emojis[index]}</div>}
-            </li>
-          ))}
-        </ul>
-      </div>
+        <div className="bg-white p-6 rounded-lg shadow-md w-full max-w-4xl text-center">
+            <h1 className="text-5xl font-galindo font-bold text-sladeOrange mb-1">
+                Registration Successful!
+            </h1>
+            <br />
+            <p className="font-galindo text-2xl">
+                Thank you for registering. Here are the login details for your
+                children:
+            </p>
+            <ul className="list-none">
+                {firstNames.map((name, index) => (
+                    <li key={`${name}-${index}`} className="my-4 text-xl">
+                        <span className="font-bold">{name}</span>
+                        {emojis[index] && <div className="text-3xl">{emojis[index]}</div>}
+                    </li>
+                ))}
+            </ul>
+        </div>
     </div>
-  );
+);
 };
 
 export default Confirmation;
