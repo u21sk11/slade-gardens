8565d8c5cd071c6f89f9cee91d294204ffeea723
Satas Kapocius <satas.kapocius.18@gmail.com>
2025-05-10 00:05:11 +0100

    feat: admin and non admin pointers

--- src/app/pages/Home.jsx
+++ src/app/pages/Home.jsx
@@ -1,62 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Authenticator } from "@aws-amplify/ui-react";
 import { fetchAuthSession } from "aws-amplify/auth";
 import { Hub } from "aws-amplify/utils";
+import Admin from "./admin/Admin";
+import Registration from "./Registration";
 
-Hub.listen("auth", async (data) => {
-  console.log(data);
-  console.log(await printUserGroups());
-});
+const Home = () => {
+    const [group, setGroup] = useState("");
+    const [loadingGroups, isLoadingGroups] = useState(true);
 
-const printUserGroups = async () => {
-  const session = await fetchAuthSession();
-  const groups = session.tokens.accessToken.payload["cognito:groups"];
+    useEffect(() => {
+        const unsubscribe = Hub.listen("auth", async (data) => {
+            if (data.payload.event === "signedIn") {
+                printUserGroups().then((result) => {
+                    console.log(result);
+                    setGroup(result);
+                });
+            }
+        });
 
-  if (groups) return groups[0];
+        return () => {
+            unsubscribe();
+        }
+    }, []);
 
-  return [];
+    const printUserGroups = async () => {
+        const session = await fetchAuthSession();
+        const groups = session.tokens.accessToken.payload["cognito:groups"];
+
+        if (groups) return groups[0];
+
+        return "";
+    };
+
+    return (
+        <div className="py-8 min-h-screen bg-gray-50 flex items-center justify-center bg-[url(./src/assets/user-login-bg.webp)] bg-cover bg-center">
+            <Authenticator>
+                {({ signOut, user }) => {
+                    if (group === "ADMINS") {
+                        return <Admin />;
+                    }
+
+                    return <Registration />;
+                }}
+            </Authenticator>
+        </div>
+    );
 };
-
-function Home() {
-  return (
-    <div className="py-8 min-h-screen bg-gray-50 flex items-center justify-center bg-[url(./src/assets/user-login-bg.webp)] bg-cover bg-center">
-      <Authenticator>
-        {({ signOut, user }) => {
-          const isAdmin = true;
-
-          if (isAdmin) {
-            return (
-              <div className="min-h-screen flex flex-col">
-                <h1 className="text-3xl font-semibold text-center mt-12">
-                  admin
-                </h1>
-                <button
-                  className="bg-blue-500 text-white px-6 py-2 rounded-full hover:bg-blue-600 transition mt-8 mx-auto"
-                  onClick={signOut}
-                >
-                  Sign Out
-                </button>
-              </div>
-            );
-          }
-
-          return (
-            <div className="min-h-screen flex flex-col">
-              <h1 className="text-3xl font-semibold text-center mt-12">
-                Not an admin
-              </h1>
-              <button
-                className="bg-blue-500 text-white px-6 py-2 rounded-full hover:bg-blue-600 transition mt-8 mx-auto"
-                onClick={signOut}
-              >
-                Sign Out
-              </button>
-            </div>
-          );
-        }}
-      </Authenticator>
-    </div>
-  );
-}
 
 export default Home;
