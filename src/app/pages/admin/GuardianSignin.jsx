f28ceccdd93ee4f80537653fb8fdd441554deb9d
Craig Scott <craig.scott.20@hotmail.com>
2025-05-26 13:16:18 +0100

    Update GuardianSignin.jsx

--- src/app/pages/admin/GuardianSignin.jsx
+++ src/app/pages/admin/GuardianSignin.jsx
@@ -1,69 +1,79 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { getGuardian } from "../../../apis/guardian";
 
-function GuardianSignin() {
-    const [email, setEmail] = useState('');
-    const [submitted, setSubmitted] = useState(false);
-    const navigate = useNavigate();  // Hook to navigate to other routes
+function GuardianSignin({ onConfirm }) {
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
 
-    // Handle email input change
-    const handleEmailChange = (e) => {
-        setEmail(e.target.value);
-    };
+  const navigate = useNavigate(); // Hook to navigate to other routes
 
-    // Handle form submission
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (email) {
-            // Optionally, store the email in localStorage, state, or pass it to the next page
-            localStorage.setItem('carerEmail', email); // Store email in localStorage for future use
+  // Handle email input change
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
 
-            // Navigate to the next page
-            setSubmitted(true);
-            navigate('/admin/who-are-you-with'); // Navigate to the next page (e.g., Who Are You With?)
-        }
-    };
+  // Handle form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError(""); // Reset error state
+    const results = await getGuardian(email);
 
-    return (
-        <div className="w-full flex items-center justify-center">
-            <div className="relative bg-white p-6 rounded-lg shadow-md w-full max-w-4xl">
-                <header className="text-center mb-4">
-                    <h2 className="text-2xl font-semibold font-galindo text-gray-800">Enter your Email</h2>
-                </header>
+    if (!results || results.data.length === 0) {
+      setError("No guardian found with this email.");
+      return;
+    }
 
-                {!submitted ? (
-                    <form onSubmit={handleSubmit} className="space-y-4">
-                        <div className="flex flex-col">
-                            <label htmlFor="email" className="text-lg font-medium text-gray-700">Email Address:</label>
-                            <input
-                                type="email"
-                                id="email"
-                                value={email}
-                                onChange={handleEmailChange}
-                                required
-                                placeholder="Enter your email"
-                                className="mt-2 p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                            />
-                        </div>
-                        <button
-                            type="submit"
-                            disabled={!email}
-                            className={
-                                "w-full py-3 bg-sladeYellow text-gray-800 font-semibold rounded-lg focus:outline-none " +
-                                (!email ? "opacity-50 cursor-not-allowed" : "hover:bg-sladeYellow-dark")
-                            }
-                        >
-                            Next
-                        </button>
-                    </form>
-                ) : (
-                    <div className="text-center">
-                        <h3 className="text-xl font-semibold text-gray-800">Thank you for submitting your email!</h3>
-                    </div>
-                )}
-            </div>
-        </div>
-    );
+    onConfirm(email);
+  };
+
+  const ErrorMessage = ({ message }) => (
+    <div className="text-red-500 text-center mb-4 font-bold">{message}</div>
+  );
+
+  return (
+    <div className="w-full flex items-center justify-center">
+      <div className="relative bg-white p-6 rounded-lg shadow-md w-full max-w-4xl">
+        <header className="text-center mb-4">
+          <h2 className="text-2xl font-semibold font-galindo text-gray-800">
+            Enter your Email
+          </h2>
+        </header>
+        {error && <ErrorMessage message={error} />}
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <div className="flex flex-col">
+            <label
+              htmlFor="email"
+              className="text-lg font-medium text-gray-700"
+            >
+              Email Address:
+            </label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={handleEmailChange}
+              required
+              placeholder="Enter your email"
+              className="mt-2 p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+          <button
+            type="submit"
+            disabled={!email}
+            className={
+              "w-full py-3 bg-sladeYellow text-gray-800 font-semibold rounded-lg focus:outline-none " +
+              (!email
+                ? "opacity-50 cursor-not-allowed"
+                : "hover:bg-sladeYellow-dark")
+            }
+          >
+            Next
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default GuardianSignin;
